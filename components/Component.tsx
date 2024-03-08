f1052601462ab91b413468446e6d06cce44ce41a
teddyaldridge <teodora.aldridge@aula.ac.uk>
2024-03-08 15:19:32 +0000

    add App.tsx and 1 component

---
+++ components/Component.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Component.css'
+
+export const Component: React.FC = () => {
+    return(
+        <h1 className='component-title'>
+            I am a Component. 
+        </h1>
+    )
+}
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from '../components/Component';
+
+function App() {
+    return (
+        <div>
+             <h1> Hello! This is a React and TS app made from scratch.</h1>
+             < Component />
+        </div>
+       
+    );
+  }
+  
+export default App;
