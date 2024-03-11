99514627616dcc1d59319cd71633de9704647dcb
teddyaldridge <teodora.aldridge@aula.ac.uk>
2024-03-11 16:51:31 +0000

    set up Prettier ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import { Component } from './components/Component'
 
 export const App: React.FC = () => {
   return (
-        <div>
-             <h1> Hello! This is a React and TS app made from scratch.</h1>
-             < Component />
-        </div>
-
+    <div>
+      <h1> Hello! This is a React and TS app made from scratch.</h1>
+      <Component />
+    </div>
   )
 }
--- src/components/Component.tsx
+++ src/components/Component.tsx
@@ -2,9 +2,5 @@ import React from 'react'
 import './Component.css'
 
 export const Component: React.FC = () => {
-  return (
-        <h1 className='component-title'>
-            I am a Component.
-        </h1>
-  )
+  return <h1 className="component-title">I am a Component.</h1>
 }
